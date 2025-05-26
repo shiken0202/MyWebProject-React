67e24427698677830ff70f8bc90b6a2ee5f4f98e
shiken0202 <hsuan0202.tw@gmail.com>
2025-05-26 17:35:00 +0800

    0526

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route,Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Cart from './pages/Cart';
 import Login from './pages/Login';
@@ -7,11 +7,12 @@ import ProductDetailPage from './pages/ProductDetailPage'
 import Collect from './pages/Collect'
 import OrderList from './pages/OrderList'
 import MyProduct from './pages/MyProduct';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePage />}/>
         <Route path="/products/user/cart" element={<Cart />} />
         <Route path="/login" element={<Login />} />
         <Route path="/products/:main/:sub" element={<ProductPage />} />
@@ -19,6 +20,7 @@ function App() {
         <Route path="/user/collect" element={<Collect />} />
         <Route path="/user/myorder" element={<OrderList />} />
         <Route path="/user/myproduct" element={<MyProduct />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
         {/* 可持續擴充更多路由 */}
       </Routes>
     </BrowserRouter>
